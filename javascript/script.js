eaa8e5a4824e568675c539a28cc2184671d4444a
Saradiaa20 <132017719+Saradiaa20@users.noreply.github.com>
2024-05-10 18:52:14 +0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -205,3 +205,15 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+// document.addEventListener('DOMContentLoaded', function() {
+//     var checkbox = document.querySelector('input[type="checkbox"]');
+//     var nav = document.querySelector('.nav1');
+
+//     checkbox.addEventListener('change', function() {
+//         if (checkbox.checked) {
+//             nav.style.display = 'block';
+//         } else if (!nav.contains(event.target) && event.target !== checkbox){
+//             nav.style.display = 'none';
+//         }
+//     });
+// });
