bbaccb0cb095b25d007809472bdde633f560cc8a
MOSTAFAGTR <168591264+MOSTAFAGTR@users.noreply.github.com>
2024-05-12 22:21:36 +0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -205,6 +205,18 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+document.addEventListener('DOMContentLoaded', function() { 
+    var checkbox = document.querySelector('input[type="checkbox"]');
+     var nav = document.querySelector('.nav1'); 
+     checkbox.addEventListener('change', function() {
+         if (checkbox.checked) { 
+            nav.style.display = 'block'; 
+        }
+         else if (!nav.contains(event.target) && event.target !== checkbox){
+             nav.style.display = 'none'; 
+            }
+         }); 
+        });
 // document.addEventListener('DOMContentLoaded', function() {
 //     var checkbox = document.querySelector('input[type="checkbox"]');
 //     var nav = document.querySelector('.nav1');
