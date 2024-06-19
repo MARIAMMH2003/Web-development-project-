32fe274aa890f97a9a94904ecf699bbaee5b3b5b
Zeina03Mohammed <152602676+Zeina03Mohammed@users.noreply.github.com>
2024-06-20 02:48:52 +0300

    user,and admin's map css update

--- javascript/map-testing.js
+++ javascript/map-testing.js
@@ -122,6 +122,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+    anchor.addEventListener('click',function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        });
+    });
+});
+
+
 
 
 
