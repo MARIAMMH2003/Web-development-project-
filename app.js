0695d0280c7f028d20d3838c63d0d3012fe3b90d
mariamshaddad <152880279+mariamshaddad@users.noreply.github.com>
2024-06-24 07:34:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,12 @@ const Museum = require('./models/museum');
 const bodyParser = require('body-parser');
 const app = express();
 const dbURI = 'mongodb+srv://new-user:abc@cluster0.ndib7gv.mongodb.net/Web-proj?retryWrites=true&w=majority&appName=Web-proj'
-
-
-mongoose.connect(dbURI)
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => {
-    app.listen(process.env.PORT || 8080);
+    app.listen(8080);
     console.log('mongodb connected successfully');
   })
+
 
   .catch(err => console.log(err));
 
