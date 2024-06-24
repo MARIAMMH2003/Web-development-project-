71a6ce3e006cf9234f7e2e77f7503823412c0202
MARIAMMH2003 <mariammhmyh@gmail.com.com>
2024-06-24 10:29:57 +0300

    highlights routing

--- models/highlight.js
+++ models/highlight.js
@@ -1,11 +1,19 @@
+// models/Highlight.js
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const highlightSchema = new Schema({
-  image: { type: String, required: true },
-  title: { type: String, required: true },
-  text: { type: String, required: true }
+const highlightSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    }
 });
 
-const Highlight = mongoose.model('Highlight', highlightSchema);
-module.exports = Highlight;
+module.exports = mongoose.model('Highlight', highlightSchema);
