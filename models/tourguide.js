d71a89f558376290c72f2928ddd4d6094985cd79
Saradiaa20 <132017719+Saradiaa20@users.noreply.github.com>
2024-06-24 03:58:47 +0300

    Update tourguide.js

--- models/tourguide.js
+++ models/tourguide.js
@@ -1,16 +1,46 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const tourGuideSchema = new mongoose.Schema({
-  name: String,
-  profession: String,
-  about: String,
-  image: String,
-  socialMedia: {
-    facebook: String,
-    twitter: String,
-    instagram: String,
-    whatsapp: String
-  },
+const tourGuideSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    profession: {
+        type: String,
+        required: true
+    },
+    about: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    // socialMedia: {
+    //     facebook: String,
+    //     twitter: String,
+    //     instagram: String,
+    //     whatsapp: String
+    // }
+    facebook: {
+        type: String,
+        required: true
+    },
+    twitter: {
+        type: String,
+        required: true
+    },
+    instagram: {
+        type: String,
+        required: true
+    },
+    whatsapp: {
+        type: String,
+        required: true
+    }
 });
 
-module.exports = mongoose.model('TourGuide', tourGuideSchema);
+const TourGuide = mongoose.model('tourguides', tourGuideSchema);
+module.exports = tourguide;
